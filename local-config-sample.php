d6c3df789acf93cde425b583b5bc30ab856c203c
Bjørn Johansen <bjorn@dekode.no>
2017-08-22 09:26:31 +0200

    Add multisite setup to config files.

--- local-config-sample.php
+++ local-config-sample.php
@@ -1,10 +1,15 @@
 <?php
 /**
  * WordPress credential configuration and setup for this installation.
- * 
- * This file should be renamed local-config.php and moved  one level up in the directory tree,
- * AFTER you have gone over all credentials.
+ *
+ * This file should be renamed local-config.php and moved one level up in the directory tree,
+ * AFTER you have gone over all credentials. You likely have to edit all of them.
  */
+
+// Multisite main site hostname.
+define( 'DOMAIN_CURRENT_SITE', 'nettsteder.regjeringen.no' );
+
+// Database credentials.
 define( 'DB_NAME', 'database_name_here' );
 define( 'DB_USER', 'username_here' );
 define( 'DB_PASSWORD', 'password_here' );
@@ -12,6 +17,7 @@ define( 'DB_HOST', 'localhost' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
+// Salts for hashing. Get these from: https://api.wordpress.org/secret-key/1.1/salt
 define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
 define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
 define( 'NONCE_KEY',        'put your unique phrase here' );
@@ -20,5 +26,5 @@ define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
 define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
 define( 'NONCE_SALT',       'put your unique phrase here' );
 
+// Debug settings.
 define( 'WP_DEBUG', true );
-
--- wp-config.php
+++ wp-config.php
@@ -10,13 +10,22 @@ define( 'WP_CONTENT_URL', 'https://' . $_SERVER['HTTP_HOST'] . '/content' );
 
 // Custom uploads directory
 define( 'UPLOADS', 'media' );
-
 // Disable the Plugin and Theme Editor
 define( 'DISALLOW_FILE_EDIT', true );
 // Disable Plugin and Theme Update and Installation
 define( 'DISALLOW_FILE_MODS', true );
 // Disable All Automatic Updates
 define( 'AUTOMATIC_UPDATER_DISABLED', true );
+
+// Multisite setup
+define( 'WP_ALLOW_MULTISITE', true );
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', false );
+$base = '/';
+// define( 'DOMAIN_CURRENT_SITE', 'nettsteder.regjeringen.no' ); // Must be set in ../local-config.php
+define( 'PATH_CURRENT_SITE', '/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
 
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
