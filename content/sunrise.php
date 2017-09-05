4ea300bbd852cbc5de0c604cd40f9e3da341b6c8
soderlind <per@soderlind.no>
2017-09-06 00:04:56 +0200

    Remove Mercator GUI

--- content/sunrise.php
+++ content/sunrise.php
@@ -2,5 +2,4 @@
 // Default mu-plugins directory if you haven't set it
 defined( 'WPMU_PLUGIN_DIR' ) or define( 'WPMU_PLUGIN_DIR', WP_CONTENT_DIR . '/mu-plugins' );
 
-require WPMU_PLUGIN_DIR . '/mercator-gui/gui.php';
 require WPMU_PLUGIN_DIR . '/mercator/mercator.php';
